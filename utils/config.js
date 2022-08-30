1aa3401e480381e776c94e453c77055b91c8ffde
Ethan Hsu <ethanhsu@MacBook-Pro.local>
2022-08-29 23:29:22 -0700

    added tests

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const PORT = process.env.PORT;
 const URL = process.env.URL;
-
+console.log(URL);
 module.exports = {
   PORT,
   URL,
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
-const dummy = (blogs) => {
-  return 1;
+// const dummy = (blogs) => {
+//   return 1;
+// };
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((prevLikes, curr) => prevLikes + curr.likes, 0);
 };
 
+const maxLikes = (blogs) => {
+  return blogs.reduce((p, n) => (p.likes > n.likes ? p : n));
+};
 module.exports = {
-  dummy,
+  //   dummy,
+  totalLikes,
+  maxLikes,
 };
